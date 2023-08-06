16a4bc05ae870b0f59479f263e234e685439e992
Gaurav Katwate <gauravkatwate5@gmail.com>
2023-08-06 14:45:05 +0530

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
-require("./db/connection");
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
 const StudentRoutes = require("./routes/StudentRoute");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -11,8 +15,23 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(StudentRoutes);
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5800;
 
 app.listen(PORT, (request, response) => {
   console.log(`server running on ${PORT} port`);
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://gauravkatwate5:gauravkatwate5@batches.nzpf9su.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
